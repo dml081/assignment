e99991cd53f5290f4d3637cd52c49bc77958bd0f
dml081 <gbadegesinadekunlesamuel@gmail.com>
2025-04-23 05:34:16 -0700

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,26 @@
+const HeroSection = () => {
+  return (
+    <section className="h-screen flex flex-col justify-center items-center px-4">
+      <div className="text-center space-y-7">
+        <p className="lg:text-[#717171] text-red-400 text-capitalize text-[14px]">
+          ALL-IN-ONE MARKETING SOFTWARE
+        </p>
+        <h1 className="lg:text-[62px] text-[40px] font-bold leading-tight">
+          Send emails, automate marketing, monetize content – in one place
+        </h1>
+        <p className="text-[18px] lg:text-black text-green-400">
+          Grow your business and boost revenue with an easy, affordable platform
+          that brings email,
+          <br /> automation, online courses and paid newsletters together.
+        </p>
+
+        <button className="bg-[#fbe30c]  px-8 py-4 rounded-3xl font-bold w-fit">
+          Start 30-day FREE trial
+        </button>
+        <p>Try us free | No credit card required | Cancel anytime</p>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
